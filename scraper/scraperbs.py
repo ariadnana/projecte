9a1917953c46fffe107cfcbeebbe32df151b741b
Ariadna <ariadnaxm@gmail.com>
2019-04-22 10:19:13 +0200

    prova scraper treure icones descripció

--- scraper/scraperbs.py
+++ scraper/scraperbs.py
@@ -77,7 +77,7 @@ while (len(concerts)!=0):
 
             desc = content.find("div", {"class": "txt-p2"})
             if(desc!=None):
-                desc = desc.get_text().encode('unicode_escape').decode('unicode_escape')
+                desc = bytes(desc.get_text(), 'utf-8').decode('utf-8', 'ignore')
                 print(desc)
 
             webcard = content.find("div", {"class": "concert-content__web"})
